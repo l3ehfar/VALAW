13b07d3299c907003fd9a615511e08f4ac520490
behnoosh mohammadzadeh <behnooshmzd@gmail.com>
2017-04-30 22:52:01 +0430

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+$(function () {
+
+    function initMap() {
+
+        var location = new google.maps.LatLng(36.3145, 59.5665);
+
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: location,
+            zoom: 16,
+            panControl: false,
+            scrollwheel: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+
+        var markerImage = 'marker.png';
+
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            icon: markerImage
+        });
+
+        var contentString = '<div class="info-window">' +
+                '<h3>MY PLACE</h3>' +
+                '<div class="info-content">' +
+                '<p>Vahdat 9-Vahdat street<br>Khayyam blv-Mashhad</p>' +
+                '</div>' +
+                '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 400
+        });
+
+        marker.addListener('click', function () {
+            infowindow.open(map, marker);
+        });
+
+    }
+
+    google.maps.event.addDomListener(window, 'load', initMap);
+});
